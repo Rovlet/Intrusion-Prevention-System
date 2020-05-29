eb335f6f0ba38aacd3369739e3fdb968a41ab1d2
kaneron676 <58699068+kaneron676@users.noreply.github.com>
2020-05-29 12:24:55 +0200

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,12 +5,16 @@ from time import time
 class Solution:
     def __init__(self):
         self.att_count = []
+        self.last_ip = '1.1.1.1'
 
     def block_address(self, source_ip):
-        # p = subprocess.Popen(["iptables", "-t", "filter", "-A", "INPUT", "-s", "$".format(source_ip), "-j", "REJECT"],
-        #                      stdout=subprocess.PIPE)
-        # output, err = p.communicate()
+        if source_ip == self.last_ip:
+            return 0
+        p = subprocess.Popen(["iptables", "-t", "filter", "-A", "INPUT", "-s", "$".format(source_ip), "-j", "REJECT"],
+                              stdout=subprocess.PIPE)
+        output, err = p.communicate()
         print("Address {} blocked".format(source_ip))
+        self.last_ip = source_ip
         return source_ip
 
     def block_after_count(self, source_ip):
