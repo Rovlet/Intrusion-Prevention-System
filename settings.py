6d4f0a95cd30035354f178234c5bc0806739ed8e
Rovlet <47760464+Rovlet@users.noreply.github.com>
2021-11-10 16:31:44 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,18 @@
+# Alerts from snort/suricata
 ALERT_FILE = 'alert'
+
+# Send email to
+ADMIN_EMAIL = ''
+
+# Send email from
+IPS_EMAIL = ''
+IPS_EMAIL_PASSWORD = ''
+
+# File to storenew rules
+IPTABLES_FILE = 'iptables'
+
+# After this time, some periodic actions will occur
+PERIODIC_ACTION_TIME = 60
+
+# Send email from this port
+APPLICATION_PORT = 465
